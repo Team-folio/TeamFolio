0376735ba0e42cc6ff9d83d4dee5fb01dd6e53ff
Maurice Emmanuel Nyanja <68531415+mauricenyanja@users.noreply.github.com>
2023-07-19 20:43:49 +0300

    added aliases 🎨

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import * as path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    // eslint-disable-next-line no-undef
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+});

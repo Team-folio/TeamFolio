9e97d293a74a9f893bf93d070146d8006526da9c
mauricenyanja <mauricee423@gmail.com>
2023-07-20 17:08:38 +0300

    setuphero icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 
+import { BeakerIcon } from '@heroicons/react/24/solid'
 
 function Navbar() {
   return (
-      <div className="bg-indigo-500"> Navbar</div>
+    <div>
+      <BeakerIcon className="h-6 w-6 text-blue-500" />
+      <p>...</p>
+    </div>
   )
 }
 

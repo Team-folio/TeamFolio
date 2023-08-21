cea77484f2f3f9fec7ba58b6a69a590906e49b7e
VhugoJC <vic_360.2000@hotmail.com>
2023-08-21 11:14:23 -0600

    Refactor SignUp

--- src/components/SignInUpView.jsx
+++ src/components/SignInUpView.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 function SignInUpView({image, form}) { // image
   
   return (
-    <div className="flex flex-col md:flex-row h-screen">
+    <div className="flex flex-col md:flex-row" style={{height:"calc(100vh - 56px)"}}>
     <div  className="md:w-1/2 h-1/3 md:h-auto bg-cover bg-center"
     style={{ backgroundImage: `url(${image})` }}
     >    
     </div>
     <div className="md:w-3/5 p-10 md:p-20">
-      {form}
+      {form()} {/* use function in order to avoid a React child error */}
     </div>
   </div>
   )

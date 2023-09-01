a508fae15f6ec31806b5ee73d48bfccb082ea10d
nutcas3 <mauricee423@gmail.com>
2023-09-01 13:12:56 +0300

    ui changes 🎨

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,8 @@
-
 import React, { useState } from "react";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { FiMenu } from "react-icons/fi";
-import { About, Home, Jobs, Projects, SignIn, SignUp } from "../pages"
-import Search from './ui/Search';
-
+import { About, Home, Jobs, Projects, SignIn, SignUp } from "../pages";
+import Search from "./ui/Search";
 
 function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
@@ -14,29 +12,43 @@ function Navbar() {
   };
   return (
     <BrowserRouter>
-      <header
-        className='fixed bg-white h-14 w-full shadow-md  px-2 py-2 md:px-4'>
-        <div className='flex items-center justify-between'>
+      <header className="fixed bg-white h-14 w-full shadow-md  px-2 py-2 md:px-4">
+        <div className="flex items-center justify-between">
           {/**<BeakerIcon className="h-6 w-6 text-blue-500" />*/}
-          <div className='flex items-center md:gap-5 gap-2'>
-            <Link to="/"
-              className='h-10 flex items-center '>
-              Logo
+          <div className="flex items-center md:gap-5 gap-2">
+            <Link to="/" className="h-10 flex items-center ">
+              {/* Logo */}
             </Link>
             {/* Navigation Links */}
-            <nav className="hidden md:flex items-center md:gap-10 font-semibold">
-              <Link to="/jobs" className="text-black hover:underline">Jobs</Link>
-              <Link to="/projects" className="text-black hover:underline">Projects</Link>
-              <Link to="/about" className="text-black hover:underline">About</Link>
+            <nav className="hidden md:flex  flex-row items-center justify-start gap-20">
+              <Link to="/jobs" className="text-black active:blue">
+                Jobs
+              </Link>
+              <Link to="/projects" className="text-black ">
+                Projects
+              </Link>
+              <Link to="/about" className="text-black ">
+                About
+              </Link>
+
+              <div className="items-center justify-start overflow-hidden flex flex-row">
+                {/* Search Input */}
+                <Search />
+              </div>
             </nav>
-            {/* Search Input */}
-            <Search />
           </div>
 
           {/* Sign In and Sign Up */}
           <nav className="hidden md:flex items-center gap-8 ml-4 font-semibold px-5">
-            <Link to="/signin" className="text-black bg- px-4 py-2">Sign In</Link>
-            <Link to="/signup" className="bg-black text-white rounded-full h-25  gap-8 self-stretch rounded-64 flex flex-row py-[0.75rem] px-[1.5rem] items-center w-50">Sign Up</Link>
+            <Link to="/signin" className="text-black bg- px-4 py-2">
+              Sign In
+            </Link>
+            <Link
+              to="/signup"
+              className="bg-black text-white rounded-full h-25  gap-8 self-stretch rounded-64 flex flex-row py-[0.75rem] px-[1.5rem] items-center w-50"
+            >
+              Sign Up
+            </Link>
           </nav>
         </div>
         {/* Mobile Menu */}
@@ -48,27 +60,39 @@ function Navbar() {
         </div>
         {showMenu && (
           <div className="md:hidden bg-white font-semibold text-right mt-4">
-            <Link to="/jobs" className="block text-black mt-2 ">Jobs</Link>
-            <Link to="/projects" className="block text-black mt-2 ">Projects</Link>
-            <Link to="/about" className="block text-black mt-2 ">About</Link>
-            <Link to="/signin" className="block text-black mt-2 ">Sign In</Link>
-            <Link to="/signup" className="block bg-black text-white rounded-md h-25  gap-8 self-stretch rounded-64  flex-row py-[0.75rem] px-[1.5rem] items-center w-50">Sign Up</Link>
+            <Link to="/jobs" className="block text-black mt-2 ">
+              Jobs
+            </Link>
+            <Link to="/projects" className="block text-black mt-2 ">
+              Projects
+            </Link>
+            <Link to="/about" className="block text-black mt-2 ">
+              About
+            </Link>
+            <Link to="/signin" className="block text-black mt-2 ">
+              Sign In
+            </Link>
+            <Link
+              to="/signup"
+              className="block bg-black text-white rounded-md h-25  gap-8 self-stretch rounded-64  flex-row py-[0.75rem] px-[1.5rem] items-center w-50"
+            >
+              Sign Up
+            </Link>
           </div>
         )}
       </header>
       <div className="pt-14">
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/About" element={<About />} />
-            <Route path="/Projects" element={<Projects />} />
-            <Route path="/Jobs" element={<Jobs />} />
-            <Route path="/SignIn" element={<SignIn />} />
-            <Route path="/SignUp" element={<SignUp />} />
-          </Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/Jobs" element={<Jobs />} />
+          <Route path="/SignIn" element={<SignIn />} />
+          <Route path="/SignUp" element={<SignUp />} />
+        </Routes>
       </div>
     </BrowserRouter>
-
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/components/ui/Search.jsx
+++ src/components/ui/Search.jsx
@@ -2,26 +2,26 @@
 
 function Search() {
   return (
-      <label className="relative block">
-          <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-              <svg
-                  className="h-5 w-5 fill-black"
-                  xmlns="http://www.w3.org/2000/svg"
-                  x="0px"
-                  y="0px"
-                  width="30"
-                  height="30"
-                  viewBox="0 0 30 30"
-              >
-                  <path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"></path>
-              </svg>
-          </span>
-          <input
-              className="w-full bg-white placeholder:italic border border-slate-300 rounded-full py-2 pl-10 pr-4 focus:outline-none  "
-              placeholder="Search"
-              type="text"
-          />
-      </label>
+    <label className="relative block ">
+    <span className="absolute inset-y-0 -0 flex items-center pl-3">
+      <svg
+        className="h-5 w-5 fill-black"
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        width="50"
+        height="30"
+        viewBox="0 0 30 30"
+      >
+        <path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"></path>
+      </svg>
+    </span>
+    <input
+      className="w-full bg-white placeholder:italic border border-slate-300 rounded-md py-2 pl-10 pr-4 focus:outline-none  "
+      placeholder="Search"
+      type="text"
+    />
+  </label>
   )
 }
 
